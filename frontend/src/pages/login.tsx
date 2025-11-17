26db0be10460c5378710d467c0d740959110752b
gakita <raulgru5@gmail.com>
2025-11-17 00:20:19 -0300

    Ajustado padding do campo de senha, reduzida animação do botão de login e convertido link de registro para elemento âncora

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -25,7 +25,7 @@ export default function LoginPage() {
         </div>
         <div className="flex flex-col gap-2">
           <p className="text-white font-inherit tracking-wide">Senha</p>
-          <div className="flex items-center gap-2 border border-slate-400 rounded-lg !py-1 justify-between flex-wrap !px-2">
+          <div className="flex items-center gap-2 border border-slate-400 rounded-lg !p-2 justify-between flex-wrap">
             <KeyIcon />
           <input
             type={showPassword ? "text" : "password"} 
@@ -36,8 +36,8 @@ export default function LoginPage() {
           </div>
           </div>
         </div>
-        <button type="submit" className="bg-blue-500 text-white !p-4 rounded-lg cursor-pointer hover:bg-blue-700 transition-colors hover:-translate-y-3 transition-transform">Entrar na aventura</button>
-        <p className="text-center">Novo por aqui?<span className="text-blue-500 cursor-pointer hover:underline hover:text-blue-700 transition-colors"> Crie a sua conta</span></p>
+        <button type="submit" className="bg-blue-500 text-white !p-4 rounded-lg cursor-pointer hover:bg-blue-700 transition-colors hover:-translate-y-1 transition-transform">Entrar na aventura</button>
+        <p className="text-center">Novo por aqui? <a href="/register" className="text-blue-500 cursor-pointer hover:underline hover:text-blue-700 transition-colors">Crie a sua conta</a></p>
       </div>
     </div>
   );
