949705232cac7d85b55df6e73262096d3f45ebe8
gakita <raulgru5@gmail.com>
2025-11-17 17:27:11 -0300

    Movida rota de login para /auth/login e adicionada HomePage como rota raiz

--- frontend/src/routes/App.tsx
+++ frontend/src/routes/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../pages/login";
+import HomePage from "../pages/HomePage";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Login />} />
+                <Route path="/auth/login" element={<Login />} />
+                <Route path="/" element={<HomePage />} />
             </Routes>
         </BrowserRouter>
     )
