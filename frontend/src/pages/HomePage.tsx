a29d41a11ada87b87b5ff021d0818cf9999f23c3
gakita <raulgru5@gmail.com>
2025-11-18 17:51:23 -0300

    Adicionado componente HeroContent à HomePage e envolvido Navbar e HeroContent em div com classe main-content

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,13 @@
 import Navbar from "../components/Navbar";
+import { HeroContent } from "../components/HeroContent";
 
 export default function HomePage() {
-    return (
+  return (
+    <>
+      <div className="main-content">
         <Navbar />
-    );
+        <HeroContent />
+      </div>
+    </>
+  );
 }
