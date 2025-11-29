a07959dbd79b2894fef826107e967780796774e9
gakita <raulgru5@gmail.com>
2025-11-29 15:21:57 -0300

    Remoção de botão de teste e adição do mongodb ao projeto

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -37,16 +37,6 @@ export default function HomePage() {
       <div className="main-content">
         <Navbar />
         <HeroContent />
-
-        {/* 🔵 Botão de teste */}
-        <div className="!p-4 text-center">
-          <button
-            onClick={enviarDadosTeste}
-            className="text-white bg-blue-600 rounded-lg mt-4 !px-9 !py-4 cursor-pointer hover:bg-blue-700 hover:shadow-md hover:shadow-blue-800"
-          >
-            🔧 Enviar dados de teste para o servidor Java
-          </button>
-        </div>
       </div>
 
       <main>
